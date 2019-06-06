a37d0be330941499cd07d61d1c2b3fba3048ea50
hhp1614 <hhp1614@gmail.com>
2019-06-06 18:12:21 +0800

    clear

---
+++ theme/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="footer">
+    <div class="sp-copyright">
+      {{ copyRight }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    copyRight() {
+      return this.$themeConfig.copyRight
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+body
+  padding-bottom 1.8rem
+.footer
+  width 100%
+  height 1.8rem
+  position fixed
+  left 0
+  bottom 0
+  padding 0 4vmin
+  background var(--bg-color)
+  z-index 9999
+  box-shadow 1px 0 15px #adadad
+.sp-copyright
+  text-align center
+</style>
---
+++ theme/components/Header.vue
@@ -0,0 +1,127 @@
+<template>
+  <header class="header">
+    <h1>{{ $themeConfig.title }}</h1>
+    <div class="nav">
+      <div
+        class="nav-item"
+        v-for="(item, index) in $themeConfig.sideBar"
+        :class="{ 'nav-item-active': item.link === $saber.$route.fullPath }"
+        :key="index"
+      >
+        <div class="nav-item-bg-1"></div>
+        <div class="nav-item-bg-2"></div>
+        <div class="nav-item-bg-3"></div>
+        <div class="nav-item-main">
+          <saber-link :to="item.link">{{ item.text }}</saber-link>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  created() {
+    // console.log(this)
+  }
+}
+</script>
+
+<style lang="stylus">
+headerHeight = 10vmin
+btnBgColor = #99999980
+color = rgb(255,87,34)
+
+body
+  padding-top headerHeight
+
+.nav-item-bg
+  position absolute
+  width 60%
+  height 60%
+  background btnBgColor
+  z-index -1
+  transition all 800ms
+
+.header
+  display flex
+  width 100%
+  height: headerHeight
+  position fixed
+  top 0
+  left 0
+  padding 0 4vmin
+  background var(--bg-color)
+  z-index 9999
+  box-shadow 0 1px 15px #adadad
+  h1
+    line-height headerHeight
+    color color
+  .nav
+    flex 1
+    display flex
+    justify-content flex-end
+    height headerHeight
+    &-item
+      display flex
+      justify-content center
+      align-items center
+      position relative
+      width headerHeight
+      margin-left 0.5vmin
+      &-bg-1
+        @extend .nav-item-bg
+        transform rotateZ(0deg)
+        animation rotateMain1 5s linear infinite
+      &-bg-2
+        @extend .nav-item-bg
+        transform rotateZ(-30deg)
+        animation rotateMain2 5s linear infinite
+      &-bg-3
+        @extend .nav-item-bg
+        transform rotateZ(-60deg)
+        animation rotateMain3 5s linear infinite
+      &-main
+        width 100%
+        height 100%
+      a
+        display flex
+        justify-content center
+        align-items center
+        width 100%
+        height 100%
+        color #586e75
+        transition all 500ms
+    &-item:hover, &-item-active
+      .nav-item-bg-1
+        background #ff572280
+        animation-duration 1s
+      .nav-item-bg-2
+        background #ff572280
+        animation-duration 1s
+      .nav-item-bg-3
+        background #ff572280
+        animation-duration 1s
+      a
+        color var(--bg-color)
+@keyframes rotateMain1
+  from
+    transform rotateZ(0deg)
+  to
+    transform rotateZ(360deg)
+@keyframes rotateMain2
+  from
+    transform rotateZ(-30deg)
+  to
+    transform rotateZ(330deg)
+@keyframes rotateMain3
+  from
+    transform rotateZ(-60deg)
+  to
+    transform rotateZ(300deg)
+@media (max-width 768px)
+  .header
+    font-size 0.8rem
+    h1
+      display none
+</style>
