59e11dfceaf3412eabb79da73f5daf8c2989a63c
hhp1614 <hhp1614@qq.com>
2023-05-29 17:33:07 +0800

    fix: 重写 rollup 打包方法，解决 GitHub Pages 404 问题

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,23 @@ import { defineConfig, PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
 
+/**
+ * 重写 rollup 打包方法，解决 GitHub Pages 404 问题
+ * @see https://github.com/vitejs/vite/issues/9119
+ * @param name 文件名
+ * @returns 标准化后的文件名
+ */
+function sanitizeFileName(name: string) {
+    const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+    const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+    const match = DRIVE_LETTER_REGEX.exec(name);
+    const driveLetter = match ? match[0] : '';
+
+    // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+    // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+    return driveLetter + name.substring(driveLetter.length).replace(INVALID_CHAR_REGEX, '');
+}
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     const plugins: PluginOption = [vue()];
@@ -63,5 +80,19 @@ export default defineConfig(({ mode }) => {
             },
         },
         plugins,
+        build: {
+            outDir: 'dist',
+            emptyOutDir: true,
+            rollupOptions: {
+                output: {
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            return id.split('/node_modules/').pop()?.split('/')[0];
+                        }
+                    },
+                    sanitizeFileName,
+                },
+            },
+        },
     };
 });
